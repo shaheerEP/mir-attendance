b39e340e9dddb462c4a572f55497363892bcb141
shaheerep <shaheerep121@gmail.com>
2026-02-05 12:12:39 +0530

    ji

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Prevent capturing binary deps in the bundle
+  serverExternalPackages: ['canvas', 'face-api.js'],
 };
 
 export default nextConfig;
