7a6b9d272552580c4d5499d12c227663c02b9a37
shaheerep <shaheerep121@gmail.com>
2026-02-05 23:02:58 +0530

    repeat

--- src/models/Class.ts
+++ src/models/Class.ts
@@ -1,12 +1,32 @@
 import mongoose, { Schema, Document, Model } from 'mongoose';
 
+export interface IPeriod {
+    subject: string;
+    startTime: string; // HH:mm
+    endTime: string;   // HH:mm
+}
+
+export interface IDaySchedule {
+    day: string;
+    periods: IPeriod[];
+}
+
 export interface IClass extends Document {
     name: string;
+    schedule: IDaySchedule[];
     created_at: Date;
 }
 
 const ClassSchema: Schema = new Schema({
     name: { type: String, required: true, unique: true },
+    schedule: [{
+        day: { type: String },
+        periods: [{
+            subject: { type: String },
+            startTime: { type: String },
+            endTime: { type: String }
+        }]
+    }],
     created_at: { type: Date, default: Date.now },
 });
 
